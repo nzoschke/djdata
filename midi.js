575825fa9adac8c0e060f4e3a9b3a9937542022f
Noah Zoschke <nzoschke@gmail.com>
2014-01-24 06:49:02 -0800

    new synchronization strategy: buffer midi load events and call back in dtrace consumer

--- midi.js
+++ midi.js
@@ -1,17 +1,20 @@
 #!/usr/bin/env node
 console.log("djdata.js start")
 
+var events    = require("events");
+var emitter   = new events.EventEmitter();
 var fs        = require("fs");
 var libdtrace = require("libdtrace");
 var midi      = require("midi");
 var mm        = require("musicmetadata");
 var path      = require("path");
 
-var dSrc      = 'syscall::open*:entry /execname == "Traktor"/ { @[copyinstr(arg0)] = count(); }'
+var dSrc      = 'syscall::open*:entry /execname == "Traktor"/ { @[copyinstr(arg0)] = max(timestamp); }'
 var fileExts  = [".mp3", ".wav", ".aiff", ".flac", ".ogg", ".wma", ".aac"]
 var portName  = "djdata.js"
 
 // variables for tracking global state
+var loadMessages = [];
 var decks = {};
 var files = [];
 
@@ -24,8 +27,12 @@ console.log("dtrace probe go");
 // every 100ms consume dtrace into shared files buffer
 setInterval(function () {
   dtp.aggwalk(function (id, key, val) {
-    files.unshift(key[0])
-    files.length = 100
+    if (fileExts.indexOf(path.extname(key[0])) == -1) return;
+    console.log(id, key, val)
+    if (loadMessages.length > 0) {
+      emitter.emit("openAudioFile", key[0], loadMessages)
+      loadMessages = []
+    }
   });
 }, 100);
 
@@ -41,21 +48,17 @@ input.on("message", function(deltaTime, message) {
   if (channel == 176 && value==127) { // "Deck is Loaded" channel
     console.log("track loaded assignment=" + cc)
 
-    // wait 500ms for dtrace consumer to sychnonize,
-    // then assume most recent open was the loaded track
-    setTimeout(function() {
-      for (var i = 0; i < files.length; i++) {
-        var file = files[i]
-        if (fileExts.indexOf(path.extname(file)) == -1) continue;
+    // push the message to a global queue and wait for dtrace consumer loop to callback
+    loadMessages.push(message)
+    emitter.removeAllListeners("openAudioFile")
+    emitter.once("openAudioFile", function(p, messages) {
+      console.log(p, messages)
 
-        var parser = mm(fs.createReadStream(file));
-        parser.on("metadata", function (result) {
-          console.log(result);
-        });
-
-        break;
-      }
-    }, 1000)
+      var parser = mm(fs.createReadStream(p));
+      parser.on("metadata", function (result) {
+        decks[cc] = { "artist": result.artist, "title": result.title, "load_at": Date.now() }
+      });
+    });
   }
 });
 
